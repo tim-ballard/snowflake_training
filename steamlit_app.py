57a75ab03d865e17b8e8b29f239339ac3702acce
Tim Ballard <tim-ballard@users.noreply.github.com>
2026-02-02 11:48:16 +0000

    test update

--- steamlit_app.py
+++ steamlit_app.py
@@ -21,10 +21,17 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 ingredients_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe, max_selections=5)
 
 if ingredients_list:
-    st.write(ingredients_list)
-    st.text(ingredients_list)
+    ingredients_string = ''
+    
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
-    ingredients_string = ' '.join(ingredients_list)
+
+   # st.write(ingredients_list)
+   # st.text(ingredients_list)
+
 
     my_insert_statement = f'''insert into smoothies.public.orders(ingredients, name_on_order)
         values ('{ingredients_string}','{name_on_order}');
@@ -38,6 +45,3 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 
 # nutrition info
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
